e18c25bd82713d1356cf47e5b7dfa4784bcbbc56
Cosmas Gikunju <gikunjucosmas@gmail.com>
2020-06-16 22:22:54 +0300

    App Entry

--- server/server.js
+++ server/server.js
@@ -7,8 +7,13 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const app = express();
 
+const home = require('./app/routes/home');
+const users = require('./app/routes/users');
+const rooms = require('./app/routes/rooms');
+const bookings = require('./app/routes/bookings');
+
 // var corsOptions = {
-// 	origin: 'http://localhost:8081',
+// 	origin: 'http://localhost:8080',
 // };
 // app.use(cors(corsOptions));
 
@@ -18,12 +23,33 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-	res.json({ message: 'Welcome to Parador Backend Server.' });
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
+
+/************************* 
+		Set Up DB 
+***********************/
+mongoose.connect(process.env.MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: true,
 });
+mongoose.set('debug', true);
+console.log('************************');
+console.log('DB CONNECTED');
+console.log('************************');
+/*******************************
+		Initialize Routes
+*******************************/
+app.use('/', home);
+app.use('/users', users);
+app.use('/rooms', rooms);
+app.use('/bookings', bookings);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.APP_PORT || 3000;
 app.listen(PORT, function () {
 	console.log(`Server is listening on ${PORT}`);
 });
