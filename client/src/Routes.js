a3f0a3a75f3f7ec41e92f7aaf009d59308abb296
Cosmas Gikunju <gikunjucosmas@gmail.com>
2020-06-17 06:06:57 +0300

    Client: Handle Undefined routes

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import Auth from './components/Auth/Auth';
+import Error404 from './components/Errors/Error404';
 
 import App from './containers/App';
 import Dashboard from './containers/Dashboard/Dashboard';
@@ -22,6 +23,10 @@ const Routes = () => {
 				<Route path='/publish' component={Dashboard} />
 				<Route path='/bookings' component={Dashboard} />
 				<Route path='/users' component={Dashboard} />
+
+				{/* Error 404 */}
+				<Route exact path='/404' component={Error404} />
+				<Redirect from='*' to='/404' />
 			</Switch>
 		</Router>
 	);
