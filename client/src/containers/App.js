4b9d49c584ddf44ce7a4b886d98f0f7a29068596
Cosmas Gikunju <gikunjucosmas@gmail.com>
2020-06-17 07:17:16 +0300

    Client: Work on Home

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -3,13 +3,15 @@ import React from 'react';
 import TopNav from '../components/Navigation/TopNav';
 import SignIn from '../components/Auth/SignIn';
 import Dashboard from './Dashboard/Dashboard';
+import Rooms from '../components/Rooms/Rooms';
 
 function App() {
 	return (
 		<React.Fragment>
 			<TopNav />
-			<SignIn />
-			<Dashboard />
+			{/* <SignIn /> */}
+			{/* <Dashboard /> */}
+			<Rooms />
 			<div>The App goes here</div>
 		</React.Fragment>
 	);
